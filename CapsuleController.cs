fbc97ba8eadda95f184c53e7dbacdeb7c537a354
RLucasMauti <45114459+RLucasMauti@users.noreply.github.com>
2019-04-29 21:40:49 -0400

    Add files via upload

---
+++ CapsuleController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        float translation = Input.GetAxis("Vertical") * speed;
+        float strafe = Input.GetAxis("Horizontal") * speed;
+        translation *= Time.deltaTime;
+        strafe *= Time.deltaTime;
+
+        transform.Translate(strafe, 0, translation);
+
+    }
+}
