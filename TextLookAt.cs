56729aa380f7fa02b73ae5d0d5497fb240e1329a
RLucasMauti <45114459+RLucasMauti@users.noreply.github.com>
2019-04-29 21:41:04 -0400

    Add files via upload

---
+++ TextLookAt.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLookAt : MonoBehaviour
+{
+    public GameObject go;
+    public GameObject cam;
+    private void Start()
+    {
+        cam = GameObject.Find("Camera");
+    }
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (go == null)
+        {
+            go = this.gameObject;
+        }
+
+        go.transform.parent.LookAt(cam.transform);
+        
+    }
+}
